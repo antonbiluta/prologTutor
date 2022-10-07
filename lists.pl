960cf9e9343c3237834dfaa1d7f78b3b649f1a96
Anton Biluta <AntonBiluta@gmail.com>
2022-10-07 11:55:30 +0300

    add listNumb, listNumb1

--- lists.pl
+++ lists.pl
@@ -4,3 +4,15 @@ sumListDown([H|T],S):-sumListDown(T,S1), S is S1+H.
 sumListUp(L,S) :- sumListUp(L, 0, S).
 sumListUp([], S, S):-!.
 sumListUp([H|L], S1, S):-S11 is S1 + H, sumListUp(T,S11,S).
+
+listNumb(I, [H|T], E):-(
+    integer(I)  ->  listNumb2(I, [H|T], T); 
+                    listNumb1(I, [H|T], E, 0)
+                    ).
+
+listNumb1(I, [H|T], E, IE):-
+    H is E ->
+    I is IE;
+    IE1 is IE+1, listNumb1(I, [H|T], E, IE1).
+
+listNumb1(_, [], _, _):-!.
